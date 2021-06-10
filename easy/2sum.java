4e72db81fba7e2bb643e8718a7cf7f11525cb8b3
Ming <mxmingya@gmail.com>
2021-06-09 23:13:02 -0700

    Create 2sum.java

---
+++ easy/2sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                res[0] = i;
+                res[1] = map.get(nums[i]);
+            }
+            if (map.containsKey(target - nums[i])) {
+                continue;
+            } else {
+                map.put(target- nums[i], i);
+            }
+        }
+        return res;
+    }
+}
